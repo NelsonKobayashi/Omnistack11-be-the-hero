0e9cb04284e186b60936f1023f0e74c661bcae4a
nelsonkobayashi <koba.nelson@gmail.com>
2021-10-15 16:50:35 -0300

    refazendo a aplicação do zero

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const cors = require('cors');
-const routes = require('./routes');
+const express = require("express");
+const cors = require("cors");
+const routes = require("./routes");
 
 const app = express();
 
@@ -8,36 +8,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333); // para acessar a aplicação no endereço: "localhost:3333" 
-                    // por padrão o node utiliza a porta 3333, o react utiliza a porta 3000
-
-/**
- * Rota / Recurso
- */
-/**
- * Métodos HTTP:
- * 
- * GET: Buscar uma informação do back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma inforação no back-end 
- */
-
- /**
-  * Tipos de parâmetros:
-  * 
-  * Query Params: Parâmetros nomeados enviados na rota após "?" (Filtros, paginação)
-  * Route Params: Parâmetros utilizados para identificar recursos
-  * Request Body: Corpo da requisição, utilizado para criar ou alterar recursos
-  */
-
-  /**
-   * Banco de Dados:
-   * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
-   * NoSQL: MongoDB, CouchDB, etc 
-   */
-        
-
-
-
-
+app.listen(3333);
